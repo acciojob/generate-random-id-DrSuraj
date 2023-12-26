da02718259ebae865a491371f393b913765f5a74
SURAJ <77872306+DrSuraj@users.noreply.github.com>
2023-12-26 16:56:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-function makeid(l) {
-  // write your code here
+function makeid(length) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        result += characters.charAt(randomIndex);
+    }
+
+    return result;
 }
 
-// Do not change the code below.
-const l = prompt("Enter a number.");
-alert(makeid(l));
+// Examples
+console.log(makeid(5));  // Example Output: e.g., "K3f2G"
+console.log(makeid(10)); // Example Output: e.g., "aB9EfgT2X1"
